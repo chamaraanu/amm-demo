72354b4393f59c1ed16e8ffeffb3e71515979441
Chamara Ariyarathne <chamaraanu@users.noreply.github.com>
2023-02-02 16:13:02 +1100

    using the default signer in transactions script

--- scripts/swap-transactions.js
+++ scripts/swap-transactions.js
@@ -1,19 +1,19 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-    const provider = new ethers.providers.JsonRpcProvider('https://eth-goerli.alchemyapi.io/v2/HlV6pIzIK1ZHKm_bIeznnMpYEiXzVomY')
-
-    const signer1 = new ethers.Wallet("ecb054e0114cc4b80c43ccef9b962d2468bc217cc42d08d24313c0648a697312", provider);
+    const [signer1] = await ethers.getSigners();
     const swapAddress = "0xade20C4a58D9AB7Ce0B336B29D021E294b9d83B7";
 
     const SwapTokens = await hre.ethers.getContractFactory("SwapTokens");
     const swapTokens = SwapTokens.attach(swapAddress);
 
+    // USDC 0x07865c6E87B9F70255377e024ace6630C1Eaa37F
+    // LINK 0x326C977E6efc84E512bB9C30f76E30c160eD06FB
     const Token = await hre.ethers.getContractFactory("Token");
-    const toToken = await Token.attach("0x07865c6E87B9F70255377e024ace6630C1Eaa37F"); // USDC
-    const fromToken = await Token.attach("0x326C977E6efc84E512bB9C30f76E30c160eD06FB"); // LINK
+    const toToken = await Token.attach("0x07865c6E87B9F70255377e024ace6630C1Eaa37F");
+    const fromToken = await Token.attach("0x326C977E6efc84E512bB9C30f76E30c160eD06FB");
 
-    amount1 = ethers.utils.parseUnits('0.08', 18);
+    amount1 = ethers.utils.parseUnits('0.01132', 18);
     await fromToken.connect(signer1).approve(swapTokens.address, amount1);
     await swapTokens.connect(signer1).swapExactInputSingle(amount1, fromToken.address, toToken.address, {gasLimit: 3000000});
 }
